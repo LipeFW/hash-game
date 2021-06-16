6c5d836732171dc653840aefbd6064ac61b28dbf
Eduarda Gislon <>
2021-06-15 21:25:49 -0300

    Criação métodos da classe Jogador

--- src/Jogador.java
+++ src/Jogador.java
@@ -1,13 +1,35 @@
 import java.util.Scanner;
 
 public class Jogador {
-    private char letra;
+    private final char letra = 'X';
+    private Mapa mapa;
 
     public Jogador(Mapa mapa) {
-
+        this.mapa = mapa;
     }
-
+    
     public boolean jogar(Scanner teclado) {
-        return true;
+        boolean posicaoValida = false;
+        while (!posicaoValida) {
+            System.out.println("Jogador ..");
+    	    System.out.print(" linha: ");
+    	    int linha = teclado.nextInt();
+            System.out.print(" coluna: ");
+            int coluna = teclado.nextInt();
+
+            if ((linha < 0) || (linha > 2) || (coluna < 0) || (coluna > 2)) {
+                System.out.println(" posição inválida!");
+    		} else {
+                if(this.mapa.jogar(linha, coluna, letra)) {
+                    posicaoValida = true;
+                }
+            }
+        }
+        if (mapa.verificarGanhador(letra)) {
+            System.out.println(" ... Jogador GANHOU!");
+            return true;
+        }
+        return false;
+        
     }
 }
